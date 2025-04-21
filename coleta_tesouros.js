e564bba916a1afdf76549c2a9e32fbc0680a1e4a
Pâmela Sofia <150748762+PamelaSofia@users.noreply.github.com>
2025-04-21 20:55:53 -0300

    Desafio 4 realizado

---
+++ coleta_tesouros.js
@@ -0,0 +1,30 @@
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//criar um prgrama que simule uma jornada em uma masmorra. deve-se percorrer cada sala e verificar se há tesouros ou monstros. se encontrar um tesouro, colecionará a recompensa. se encontrar um monstro, terá que derrotá-lo para continuar.
+
+// Entrada de dados. Lembre-se: O parseInt(()) é importante para a conversão dos valores de entrada(String) para um valor numérico(int).
+const totalSalas = parseInt(gets());
+//Aqui temos uma demonstração de array com valores já predefinidos:
+const salasComTesouro = [2, 4, 7];
+const salasComMonstro = [3, 6, 8];
+
+//Aqui temos uma forma de loop para percorrer cada sala da masmorra:
+for (let sala = 1; sala <= totalSalas; sala++) {
+//Utilizamos o .includes() para verificar se um número está presente nos arrays:
+    const temTesouro = salasComTesouro.includes(sala);
+    const temMonstro = salasComMonstro.includes(sala);
+
+//TODO: Agora implemente uma condição necessária para verificar e exibir se há tesouro na sala e se há montro na sala:
+    if (sala == 2) {
+        print("Tesouro na sala " + sala + "!");
+    } else if(sala == 4){
+      print("Tesouro na sala " + sala + "!");
+    } else if (sala == 7) {
+        print("Tesouro na sala " + sala + "!");
+    } else if (sala == 3){
+      print("Monstro na sala " + sala + "!");
+    } else if (sala == 6){
+      print("Monstro na sala " + sala + "!");
+    } else if (sala == 8){
+      print("Monstro na sala " + sala + "!");
+    }
+}
